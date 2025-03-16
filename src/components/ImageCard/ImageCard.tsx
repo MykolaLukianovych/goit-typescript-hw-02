461c968c32530f0432fe47ca2f84f7773c7419ee
MykolaLukianovych <lukjanovych@gmail.com>
2025-03-16 10:30:00 +0200

    fix

--- src/components/ImageCard/ImageCard.tsx
+++ src/components/ImageCard/ImageCard.tsx
@@ -1,6 +1,14 @@
+import { Image } from "../../App";
 import s from "./ImageCard.module.css"
 
-const ImageCard = ({ image, openModal }) => {
+
+type Props = {
+  image: Image;
+  openModal: (image: Image) => void;
+};
+
+
+const ImageCard = ({ image, openModal }: Props) => {
   return (
     <div className={s.imageCard} onClick={() => openModal(image)}>
       <img src={image.urls.small} alt={image.alt_description}/>
