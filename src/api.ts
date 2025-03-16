a39c85ed4b73f8c2c3cab4a05972e2d577278ad3
MykolaLukianovych <lukjanovych@gmail.com>
2025-03-16 10:24:14 +0200

    corrections

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,32 @@ import axios from "axios";
 
 const UNSPLASH_ACCESS_KEY = "R0FOGP8L2j0cDefFKxScuaaaIE43hZXwpW4fwhKUbc4";
 
-export const fetchImages = async (query, page) => {
+
+type ApiResponse = {
+  results: Array<{
+    id: string;
+    alt_description: string;
+    likes: number;
+    description: string;
+    created_at: string;
+    tags: { title: string }[];
+    urls: {
+      small: string;
+      regular: string;
+    };
+    user: {
+      name: string;
+      social: {
+        portfolio_url: string;
+      };
+      location: string;
+    };
+  }>;
+  total: number;
+  total_pages: number;
+};
+
+export const fetchImages = async (query: string, page: number): Promise<ApiResponse> => {
   try {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query, page, per_page: 12 },
